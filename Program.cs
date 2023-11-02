da50599b60623eefa076a4ebef8b6f2f5fe27d90
Bietood <gabriel.duarte69@etec.sp.gov.br>
2023-11-02 17:19:56 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿double Salariohora;
+double Horastotais;
+double Horasextras;
+double Salario;
+
+Console.Write("Digite seu salário por hora: ");
+Salariohora = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("Digite suas horas totais: ");
+Horastotais = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("Digite suas horas extras: ");
+Horasextras = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+
+Salario = (Salariohora * (Horastotais - Horasextras)) + (double)(Salariohora * Horasextras * 1.4);
+
+Console.WriteLine($"salario-hora {Salariohora:C}, {Horastotais} horas, sendo {Horasextras} horas-extra, salário = {Salario:C}");
